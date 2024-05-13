21e1af1a25fcfa6068244969454cd4f00c77de48
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-05-13 14:39:48 +0300

    localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [value, setValue] = useState(0)
+
+    const onClickHandler = () => {
+        setValue(value + 1)
+    }
+
+    const onClickHandlerSetItem = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+        localStorage.setItem('counterValue+1', JSON.stringify(value))
+    }
+
+    const onClickHandlerGetItem = () => {
+        let valueIsString = localStorage.getItem('counterValue')
+        if(valueIsString !== null){
+            let newValue = JSON.parse(valueIsString)
+            setValue(newValue)
+        }
+    }
+
+    const clearLocalStorageHandler = () => {
+        localStorage.clear()
+        setValue(0)
+    }
+
+const removeItemFromLocalStorageHandler = () => {
+        localStorage.removeItem('counterValue+1')
+    }
+
+
+    return (
+        <div className="App">
+            <div>{value}</div>
+            <button onClick={onClickHandler}>+</button>
+            <button onClick={onClickHandlerSetItem}>setToLocalStorage</button>
+            <button onClick={onClickHandlerGetItem}>getFromLocalStorage</button>
+            <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
+            <button onClick={removeItemFromLocalStorageHandler}>removeItemFromLocalStorage</button>
+        </div>
+    );
 }
 
 export default App;
