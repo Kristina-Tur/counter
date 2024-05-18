1df73554d7ce20c366424742c1fa1abecb964c0c
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-05-18 15:31:12 +0300

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,74 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Button} from './components/Button';
 import {Counter2} from './components/Counter2';
+import {Counter} from './components/Counter';
+import {CounterSetting} from './components/CounterSetting';
 
 
-//во втором каунтере один юсстэйт, а изменение юсрейф
 function App() {
     const counter = 0
     const maxCounter = 5
     const [value, setValue] = useState(0)
 
-    const INCHandler = () => {
+    const incCounter = () => {
         setValue(value + 1)
     }
 
-    const RESETHandler = () => {
+    const resetCounter = () => {
         setValue(0)
     }
 
+    const setCounter = (value: string, maxValue: string) => {
+        localStorage.setItem('counterValue', value)
+        localStorage.setItem('counterMaxValue', maxValue)
+        alert(value + maxValue)
+    }
+
+   /* useEffect(() => {
+        let valueIsString = localStorage.getItem('counterValue')
+        if (valueIsString !== null) {
+            let newValue = JSON.parse(valueIsString)
+            setValue(newValue)
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }, [value])
+
+
+    const onClickHandlerSetItem = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+        localStorage.setItem('counterValue+1', JSON.stringify(value))
+    }
+
+    const onClickHandlerGetItem = () => {
+        let valueIsString = localStorage.getItem('counterValue')
+        if (valueIsString !== null) {
+            let newValue = JSON.parse(valueIsString)
+            setValue(newValue)
+        }
+    }
+
+    const clearLocalStorageHandler = () => {
+        localStorage.clear()
+        setValue(0)
+    }
+
+    const removeItemFromLocalStorageHandler = () => {
+        localStorage.removeItem('counterValue+1')
+    }*/
+
     return (
         <div className="App">
-            <div className={'container'}>
-                <div className={`counter ${value === 5 ? 'colorCounter' : ''}`}>{value}</div>
-                <div className={'buttonWrapper'}>
-                    <Button className={'button'} title={'INC'} onClick={INCHandler} disabled={value === maxCounter}/>
-                    <Button className={'button'} title={'RESET'} onClick={RESETHandler} disabled={value === counter}/>
-                </div>
-            </div>
+            <CounterSetting setCounter={setCounter}/>
+            <Counter
+                counter={counter}
+                maxCounter={maxCounter}
+                value={value}
+                incCounter={incCounter}
+                resetCounter={resetCounter}
+            />
             <Counter2/>
         </div>
     );
